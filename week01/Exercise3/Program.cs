a665bcd0935757fe773bd14d5a27f6c379fd1df6
izelomabarak <joseluisim.lds@gmail.com>
2025-05-11 18:22:36 -0600

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {               
+        string response = "A";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int yourGuessInt  = -1;
+            int tries = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                string yourGuess = Console.ReadLine();
+                yourGuessInt = int.Parse(yourGuess);
+
+                if (magicNumber == yourGuessInt)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (magicNumber > yourGuessInt)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < yourGuessInt)
+                {
+                    Console.WriteLine("Lower");
+                }
+                tries++;
+            }while (magicNumber != yourGuessInt);
+            Console.WriteLine($"You do {tries} guesses");
+
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        }while (response == "yes");
     }
 }
